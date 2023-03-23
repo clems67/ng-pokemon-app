32d18d360205899a423bfba1924911404f3123af
CARAVITA <clement.caravita@sogeti.com>
2023-03-23 14:34:05 +0100

    set private property

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -4,19 +4,23 @@ import { Directive, ElementRef, HostListener, Input } from "@angular/core";
   selector: "[appBorderCard]",
 })
 export class BorderCardDirective {
+  private initialColor: string = "#f5f5f5";
+  private defaultColor: string = "green";
+  private defaulHeight: number = 180;
+
   constructor(private el: ElementRef) {
-    this.setHeight(180);
-    this.setBorder("#f5f5f5");
+    this.setBorder(this.initialColor);
+    this.setHeight(this.defaulHeight);
   }
 
   @Input("appBorderCard") borderColor: string;
 
   @HostListener("mouseenter") onMouseEnter() {
-    this.setBorder(this.borderColor || "green");
+    this.setBorder(this.borderColor || this.defaultColor);
   }
 
   @HostListener("mouseleave") onMouseLeave() {
-    this.setBorder("#f5f5f5");
+    this.setBorder(this.initialColor);
   }
 
   setHeight(height: number) {
