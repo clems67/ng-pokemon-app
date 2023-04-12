2461c79d415d7c1d631c3c2de5e5e7cd53f9a2f5
CARAVITA <clement.caravita@sogeti.com>
2023-04-12 14:02:33 +0200

    api routing

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -7,6 +7,7 @@ import { InMemoryDbService } from "angular-in-memory-web-api";
 })
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    return { POKEMONS };
+    const pokemons = POKEMONS;
+    return { pokemons };
   }
 }
--- src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
+++ src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Pokemon } from "../pokemon";
 import { PokemonService } from "../pokemon.service";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-detail-pokemon",
@@ -19,10 +20,14 @@ export class DetailPokemonComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.pokemonList = this.pokemonService.getPokemonList();
+    this.pokemonService
+      .getPokemonList()
+      .subscribe((pokemonList) => (this.pokemonList = pokemonList));
     const pokemonId: string | null = this.route.snapshot.paramMap.get("id");
     if (pokemonId) {
-      this.pokemon = this.pokemonService.getPokemonById(+pokemonId);
+      this.pokemonService
+        .getPokemonById(+pokemonId)
+        .subscribe((pokemon) => (this.pokemon = pokemon));
     }
     console.log(this.pokemon);
   }
